87463ede6b015ba657376aa22e5794067cf400b8
trustcoinnew <trustcoinnew@gmail.com>
2014-08-30 22:20:16 +0400

    Update main.h

--- src/main.h
+++ src/main.h
@@ -25,7 +25,7 @@ class CInv;
 class CRequestTracker;
 class CNode;
 
-static const int LAST_POW_BLOCK = 51000; //
+
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
